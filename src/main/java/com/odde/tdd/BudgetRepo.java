abdd1467ea106959acb8b29e830e5924848e890d
Altman <altman@qq.com>
2022-07-02 15:50:45 +0800

    Budget mock

--- src/main/java/com/odde/tdd/BudgetRepo.java
+++ src/main/java/com/odde/tdd/BudgetRepo.java
@@ -1,7 +1,10 @@
 package com.odde.tdd;
 
+import java.time.YearMonth;
 import java.util.List;
 
 public interface BudgetRepo {
     List<Budget> findAll();
+
+    long countBudget(YearMonth start, YearMonth end);
 }
--- src/test/java/com/odde/tdd/MockTest.java
+++ src/test/java/com/odde/tdd/MockTest.java
@@ -11,9 +11,19 @@ import static org.mockito.Mockito.when;
 
 public class MockTest {
     @Test
-    public void mock_repo(){
+    public void mock_repo() {
         BudgetRepo repo = mock(BudgetRepo.class);
         when(repo.findAll()).thenReturn(Arrays.asList(new Budget(YearMonth.of(2018, 11), 1000)));
         assertEquals(1, repo.findAll().size());
+    }
+
+    @Test
+    public void mock_count_budget() {
+        BudgetRepo repo = mock(BudgetRepo.class);
+        long budget = 7000;
+        YearMonth start = YearMonth.of(2022, 1);
+        YearMonth end = YearMonth.of(2022, 7);
+        when(repo.countBudget(start, end)).thenReturn(budget);
+        assertEquals(budget, repo.countBudget(start, end));
     }
 }
